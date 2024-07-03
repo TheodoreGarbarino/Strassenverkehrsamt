121dc3156024ffe3491778a436c00c7dfb24e578
theodore.garbarino <theodore.garbarino@edu.tbz.ch>
2024-07-03 09:11:30 +0200

    Projektdateien hinzufügen.

---
+++ Ueb11a_StrassenverkehrsamtTest/UnitTestPerson.cs
@@ -0,0 +1,14 @@
+using Ueb11a_Strassenverkehrsamt;
+
+namespace Ueb11a_StrassenverkehrsamtTest                                              {
+    [TestClass]
+    public class UnitTestPerson{
+        [TestMethod]
+        public void TestMethode_Konstruktor()
+                                                                                         {
+            Person p = new Person();
+
+            Assert.IsTrue(true);
+}//b///*/
+   }
+         }
